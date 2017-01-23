7eb0b89d60ac51115203d16f2e5fdbc87692f0d0
PCSailor <Phil@PhilCurtis.io>
2017-01-23 00:09:57 -0600

    prime_prestudy_bonus_week_04_Splinter Challenge

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
-var msg = "Hey this works!";
-var func = function() {
-  console.log(msg);
+function splinter(num1, num2) {
+	var z = num1 * num2;
+	var numArray = [num1, num2, z];
+	return numArray;
 }
-func();
+console.log(splinter(1, 2));
+/* Notes:
+Create a JavaScript file named 'script.js'.
+Add a 'splinter' function.
+The function should take in 2 numbers as arguments,
+multiply those numbers, and,
+save the result to a locally scoped variable called 'z'.
+Next, create an array that stores the first argument, second argument, and z.
+Return that array.
+Try testing this by console logging the splinter function, and,
+passing in 1 and 2.
+Check all of this in to a repo called "prime_prestudy_bonus_week_04"
+*/
